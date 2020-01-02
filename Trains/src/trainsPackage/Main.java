e8f7c5dd719d36e9baab5df45b441fdd1f9646b1
Jamie Walker <jamie.walker@students.plymouth.ac.uk>
2020-01-02 12:59:58 +0000

    still need obvious tests like string or readable

--- Trains/src/trainsPackage/Main.java
+++ Trains/src/trainsPackage/Main.java
@@ -4,26 +4,7 @@
  * and open the template in the editor.
  */
 package trainsPackage;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import org.json.*;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
-import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Map;
-import static trainsPackage.Methods.Populate;
-import static trainsPackage.Methods.getAllTrains;
-import static trainsPackage.Methods.getLateTrains;
-import static trainsPackage.Methods.getTrainsAtPlatform;
-import static trainsPackage.Methods.getTrainsAtStation;
-//import org.json.simple.JSONArray;
-//import org.json.simple.JSONObject;
-//import org.json.simple.parser.JSONParser;
 
 
 
